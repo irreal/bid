07127076ff516ebf532aab05a24abe62b014809d
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-11 18:59:20 +0100

    Added apexcharts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App + apexcharts ;)"/> -->
+  <apexchart width="500" type="bar" :options="options" :series="series"></apexchart>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import apexchart from 'vue-apexcharts';
 
 @Component({
   components: {
     HelloWorld,
+    apexchart
   },
+  data: function() {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 2010]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+}
 </script>
--- tests/e2e/specs/test.2.js
+++ tests/e2e/specs/test.2.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My Second Test', () => {
+describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.wait(3000)
+    cy.wait(5000)
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
   })
