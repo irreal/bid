d2c1d5485bb91a3e0e85d17d4102d2729e22cb37
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-25 14:35:38 +0100

    weather function

--- src/functions/weather.js
+++ src/functions/weather.js
@@ -1,10 +1,44 @@
+require("dotenv").config();
+const axios = require("axios");
 exports.handler = function(event, context, callback) {
-  // your server-side functionality
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      event: `Hello world broj 1 ${JSON.stringify(event)}`,
-      context: `Hello world broj 2 ${JSON.stringify(context)}`
+  let clientIp = event.headers["client-ip"];
+  if (!clientIp) {
+    if (process.env.FALLBACK_CLIENT_IP) {
+      clientIp = process.env.FALLBACK_CLIENT_IP;
+    } else {
+      callback(new Error("couldn't read client ip"));
+      return;
+    }
+  }
+  axios
+    .get(
+      `http://api.ipstack.com/${clientIp}?access_key=${
+        process.env.IPSTACK_KEY
+      }&fields=city,location.capital`
+    )
+    .then(resp => {
+      const city = resp.data.city || resp.data.location.capital;
+      if (!city) {
+        callback(new Error("couldn't read city from ip"));
+      }
+
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${
+            process.env.OPEN_WEATHER_MAP_KEY
+          }`
+        )
+        .then(resp => {
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(resp.data)
+          });
+        })
+        .catch(err => {
+          callback(err);
+        });
     })
-  });
+    .catch(err => {
+      callback(err);
+    });
 };
