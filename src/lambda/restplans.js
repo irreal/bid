7966022a9dbaa8e7b7da2fa9ade4c605bb2cdf0f
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-26 21:40:16 +0100

    removed lambda build output from git

--- src/lambda/restplans.js
+++ src/lambda/restplans.js
@@ -6,25 +6,33 @@ const planSchema = new mongoose.Schema({
 });
 var Plan = mongoose.model("plans", planSchema);
 export async function handler(event, context, callback) {
-  try {
-    const uri = process.env.MONGO_CONNECTION;
-    await mongoose.connect(uri, { useNewUrlParser: true });
+  return new Promise(async resolve => {
+    try {
+      const uri = process.env.MONGO_CONNECTION;
+      await mongoose.connect(uri, { useNewUrlParser: true });
 
-    var mesecni = new Plan({
-      title: "Mesečni",
-      percent_complete: 12.2,
-      date_added: new Date()
-    });
-    await mesecni.save();
+      // var mesecni = new Plan({
+      //   title: "Mesečni",
+      //   percent_complete: 12.2,
+      //   date_added: new Date()
+      // });
+      // await mesecni.save();
 
-    const results = await Plan.find();
-    console.log(results);
-    callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(results)
-    });
-  } catch (err) {
-    console.log("error accessing plans ", err);
-    callback(err);
-  }
+      const results = await Plan.find();
+      console.log(results);
+      console.log("pre cb-a");
+      await mongoose.connection.close();
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(results)
+      });
+      console.log("posle cb-a");
+      resolve();
+    } catch (err) {
+      await mongoose.connection.close();
+      console.log("error accessing plans ", err);
+      callback(err);
+      resolve();
+    }
+  });
 }
