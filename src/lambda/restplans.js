84359faca359dcb273494ac65662401c6bed99e2
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-26 23:36:41 +0100

    Added authorization. Using custom version of netlify lambda vue cli plugin until original is updated

--- src/lambda/restplans.js
+++ src/lambda/restplans.js
@@ -2,37 +2,68 @@ const mongoose = require("mongoose");
 const planSchema = new mongoose.Schema({
   title: String,
   date_added: Date,
-  percent_complete: Number
+  percent_complete: Number,
+  created_by: String,
+  allowed_roles: [String],
+  allowed_users: [String]
 });
 var Plan = mongoose.model("plans", planSchema);
 export async function handler(event, context, callback) {
-  return new Promise(async resolve => {
-    try {
-      const uri = process.env.MONGO_CONNECTION;
-      await mongoose.connect(uri, { useNewUrlParser: true });
+  const {
+    identity,
+    user
+  } = context.clientContext;
+  console.log('user je', user);
+  if (!user) {
+    callback(null, {
+      statusCode: 401,
+      body: "missing client context"
+    })
+    return;
+  }
+  try {
+    const uri = process.env.MONGO_CONNECTION;
+    await mongoose.connect(uri, {
+      useNewUrlParser: true
+    });
 
-      // var mesecni = new Plan({
-      //   title: "Mesečni",
-      //   percent_complete: 12.2,
-      //   date_added: new Date()
-      // });
-      // await mesecni.save();
+    // var mesecni = new Plan({
+    //   title: "Mesečni",
+    //   percent_complete: 12.2,
+    //   date_added: new Date()
+    // });
+    // await mesecni.save();
 
-      const results = await Plan.find();
-      console.log(results);
-      console.log("pre cb-a");
-      await mongoose.connection.close();
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(results)
-      });
-      console.log("posle cb-a");
-      resolve();
-    } catch (err) {
-      await mongoose.connection.close();
-      console.log("error accessing plans ", err);
-      callback(err);
-      resolve();
+    let filter = {};
+    if (!user.app_metadata.roles.includes('adminB')) {
+      filter = {
+        $or: [
+          {
+            created_by: user.email
+          },
+          {
+            allowed_roles: {
+              $in: user.app_metadata.roles
+            }
+          },
+          {
+            allowed_users : user.email
+          }
+        ]
+      };
     }
-  });
+    const results = await Plan.find(filter);
+    console.log(results);
+    console.log("pre cb-a");
+    await mongoose.connection.close();
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    });
+    console.log("posle cb-a");
+  } catch (err) {
+    await mongoose.connection.close();
+    console.log("error accessing plans ", err);
+    callback(err);
+  }
 }
