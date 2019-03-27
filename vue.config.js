420d082126824f114da015ef3177700db422e3fe
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-27 11:19:31 +0100

    fixed vue config format

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "pwa.themeColor": "#FAA634",
-  "pwa.iconPaths" : {
-    favicon32: 'img/icons/favicon-32x32.png',
-    favicon16: 'img/icons/favicon-16x16.png',
-    appleTouchIcon: 'img/icons/apple-touch-icon.png',
-    maskIcon: 'img/icons/safari-pinned-tab.svg',
-    msapplication: '/img/icons/browserconfig.xml'
+  pwa: {
+    themeColor: "#FAA634",
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msapplication: "/img/icons/browserconfig.xml"
+    }
   }
-}
+};
