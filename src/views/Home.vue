00c56dfcb6322b8c577a2d1ed9887d8cc07ead41
Miloš Spasojević <milos.spasojevic.tms@gmail.com>
2019-03-31 11:36:12 +0200

    Added vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,13 @@
 <template>
-  <div class="d-flex w-100 h-100 justify-content-center align-items-center">
-    <div class="d-flex flex-column justify-content-between h-25 w-25">
-      <button class="btn-primary" href @click.prevent="$emit('login')">
-        Uloguj se
-      </button>
-      <button class="btn-success" href @click.prevent="$emit('signup')">
-        Registruj se
-      </button>
-      <button class="btn-danger" href @click.prevent="$emit('logout')">
-        Izloguj se
-      </button>
-    </div>
-  </div>
+  <HelloWorld />
 </template>
 
 <script>
-export default {
-  name: "home"
-};
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    components: {
+      HelloWorld
+    }
+  }
 </script>
-<style scoped></style>
